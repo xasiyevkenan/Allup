866ec15e00858c3acac41a15bdf49141cf700598
Kanan Xasiyev <121182410+xasiyevkenan@users.noreply.github.com>
2023-07-19 19:39:41 +0400

    Update ContactViewModel.cs

--- Allup/ViewModels/ContactViewModel.cs
+++ Allup/ViewModels/ContactViewModel.cs
@@ -4,8 +4,8 @@ namespace Allup.ViewModels
 {
     public class ContactViewModel
     {
-        public List<Contact> Contacts { get; set; }
         public List<Brand> Brands { get; set; }
         public List<Banner> Banners { get; set; }
+        public List<Contact> Contacts { get; set; }
     }
 }
