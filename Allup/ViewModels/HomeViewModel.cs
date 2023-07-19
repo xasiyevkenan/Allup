1f460034efe37e0fbc3d64b71729d902158a8905
Kanan Xasiyev <121182410+xasiyevkenan@users.noreply.github.com>
2023-07-19 19:40:19 +0400

    comm

--- Allup/ViewModels/HomeViewModel.cs
+++ Allup/ViewModels/HomeViewModel.cs
@@ -4,9 +4,9 @@ namespace Allup.ViewModels
 {
     public class HomeViewModel
     {
-        public List<Category> Categories { get; set; }
-        public List<Product> Products { get; set;}
-        public List<Brand> Brands { get; set;}
         public List<Banner> Banners { get; set;}
+        public List<Product> Products { get; set;}
+        public List<Category> Categories { get; set; }
+        public List<Brand> Brands { get; set;}
     }
 }
