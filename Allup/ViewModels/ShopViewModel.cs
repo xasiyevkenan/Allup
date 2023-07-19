dc77fba5058aff9181a23f62af596844a9a8a1e7
Kanan Xasiyev <121182410+xasiyevkenan@users.noreply.github.com>
2023-07-19 19:41:17 +0400

    firstcommit

--- Allup/ViewModels/ShopViewModel.cs
+++ Allup/ViewModels/ShopViewModel.cs
@@ -4,8 +4,8 @@ namespace Allup.ViewModels
 {
     public class ShopViewModel
     {
+        public List<Banner> Banners { get; set; }
         public List<Product> Products { get; set; }
         public List<Brand> Brands { get; set; }
-        public List<Banner> Banners { get; set; }
     }
 }
